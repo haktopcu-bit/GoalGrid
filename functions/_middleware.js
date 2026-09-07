21b2c69d61430b2976fcdff189a70cbc3b4c9a80
haktopcu-bit <haktopcu@gmail.com>
2026-09-07 19:57:21 +0300

    Load GoalGrid v1.11 domestic-history fix

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -3,7 +3,7 @@ export async function onRequest(context) {
   const type = response.headers.get('content-type') || '';
   if (!type.includes('text/html')) return response;
   return new HTMLRewriter()
-    .on('head', { element(el) { el.append('<link rel="stylesheet" href="/goalgrid-v111.css?v=1110">', { html: true }); } })
-    .on('body', { element(el) { el.append('<script src="/goalgrid-v111.js?v=1110"></script>', { html: true }); } })
+    .on('head', { element(el) { el.append('<link rel="stylesheet" href="/goalgrid-v111.css?v=1111">', { html: true }); } })
+    .on('body', { element(el) { el.append('<script src="/goalgrid-v111.js?v=1111"></script><script src="/goalgrid-v111-fix.js?v=1111"></script>', { html: true }); } })
     .transform(response);
 }
