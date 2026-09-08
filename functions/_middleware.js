fc74eacaa02755cacbb4da47b448aec3130ec0ff
haktopcu-bit <haktopcu@gmail.com>
2026-09-08 15:30:00 +0300

    Load GoalGrid v1.11.6 history and win-highlight fix

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -3,7 +3,7 @@ export async function onRequest(context) {
   const type = response.headers.get('content-type') || '';
   if (!type.includes('text/html')) return response;
   return new HTMLRewriter()
-    .on('head', { element(el) { el.append('<link rel="stylesheet" href="/goalgrid-v111.css?v=1115">', { html: true }); } })
-    .on('body', { element(el) { el.append('<script src="/goalgrid-v111.js?v=1115"></script><script src="/goalgrid-v111-fix.js?v=1115"></script><script src="/goalgrid-v1112-fix.js?v=1115"></script><script src="/goalgrid-v1113-fix.js?v=1115"></script><script src="/goalgrid-v1114-ui.js?v=1115"></script><script src="/goalgrid-v1115-fix.js?v=1115"></script>', { html: true }); } })
+    .on('head', { element(el) { el.append('<link rel="stylesheet" href="/goalgrid-v111.css?v=1116">', { html: true }); } })
+    .on('body', { element(el) { el.append('<script src="/goalgrid-v111.js?v=1116"></script><script src="/goalgrid-v111-fix.js?v=1116"></script><script src="/goalgrid-v1112-fix.js?v=1116"></script><script src="/goalgrid-v1113-fix.js?v=1116"></script><script src="/goalgrid-v1114-ui.js?v=1116"></script><script src="/goalgrid-v1115-fix.js?v=1116"></script><script src="/goalgrid-v1116-fix.js?v=1116"></script>', { html: true }); } })
     .transform(response);
 }
